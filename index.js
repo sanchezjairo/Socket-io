c466ae40f73b8eb3bff2ab6bd1f5acf3eedd6fad
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2021-10-08 22:17:06 -0700

    Added username feature

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var numb = 1;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+    socket.emit('name', "friend"+numb);
+    numb++;
+
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
